1bb446b06a72e9db99aea8866a648c493c670c4a
luan <hi@luanhailiang.cn>
2020-12-25 19:31:06 +0800

    rm font

--- kiosk/alt_start.sh
+++ kiosk/alt_start.sh
@@ -7,8 +7,6 @@ if [ ! -z ${CONTROL_TV+x} ] && [ "$CONTROL_TV" -eq "1" ]
 fi
 
 
-apt-get install ttf-bitstream-vera
-
 cd /home/chromium/tohora && ./tohora 8080 /home/chromium/launch.sh &
 
 wget --post-data "url=$LAUNCH_URL" http://localhost:8080/launch/ >/dev/null 2>&1 && sleep infinity
